c08a0a923ee482eaa404029eb1c1aca68907053b
Pili <pilarriusm@gmail.com>
2026-03-08 10:55:08 +0100

    Fix handler function signature for Netlify

--- api/handler.py
+++ api/handler.py
@@ -1,7 +1,11 @@
 """
 Netlify Functions handler entry point
+Re-exports the Mangum handler from main_talendeur
 """
 from main_talendeur import handler
 
-# Netlify will call this handler
-__all__ = ['handler']
+# This is what Netlify will call
+# The handler is already the Mangum-wrapped FastAPI app
+def handler(event, context):
+    from main_talendeur import handler as mangum_handler
+    return mangum_handler(event, context)
