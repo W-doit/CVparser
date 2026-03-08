575391967a48a29e7f632a7eca784432ca112328
Pili <pilarriusm@gmail.com>
2026-03-08 11:28:47 +0100

    Fix: Bundle all Python modules into netlify/functions directory

--- netlify/functions/handler.py
+++ netlify/functions/handler.py
@@ -2,14 +2,8 @@
 Netlify Python Function Handler for FastAPI via Mangum
 This is the entry point that Netlify calls
 """
-import sys
-import os
-
-# Add root directory to Python path so we can import our modules
-root_dir = os.path.join(os.path.dirname(__file__), '..', '..')
-sys.path.insert(0, root_dir)
-
-# Now import the Mangum-wrapped FastAPI handler from main_talendeur
+# Import the Mangum-wrapped FastAPI handler from main_talendeur
+# (all modules are now in the same directory)
 from main_talendeur import handler as app_handler
 
 # Netlify will call this function
