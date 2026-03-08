bd3788c149d3c43aa789a99c5320995dcbbce371
Pili <pilarriusm@gmail.com>
2026-03-08 11:56:41 +0100

    Simplify handler and add __init__.py

--- netlify/functions/handler.py
+++ netlify/functions/handler.py
@@ -1,15 +1,4 @@
 """
 Netlify Python Function Handler for FastAPI via Mangum
-This is the entry point that Netlify calls
 """
-# Import the Mangum-wrapped FastAPI handler from main_talendeur
-# (all modules are now in the same directory)
-from main_talendeur import handler as app_handler
-
-# Netlify will call this function
-def handler(event, context):
-    """
-    Netlify function entry point
-    Delegates to the Mangum handler which wraps our FastAPI app
-    """
-    return app_handler(event, context)
+from main_talendeur import handler
